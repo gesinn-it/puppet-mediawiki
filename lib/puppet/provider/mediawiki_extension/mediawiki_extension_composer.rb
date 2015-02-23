fe1d2e772b00a59f9abece49f42690e2cc75aaa9
gesinn.it <github@gesinn.it>
2015-02-23 18:12:45 +0100

    Update mediawiki_extension_composer.rb

--- lib/puppet/provider/mediawiki_extension/mediawiki_extension_composer.rb
+++ lib/puppet/provider/mediawiki_extension/mediawiki_extension_composer.rb
@@ -31,7 +31,7 @@ Puppet::Type.type(:mediawiki_extension_composer).provide(:mediawiki_extension_co
 
 
   def create
-    composer('require', $source, $source_version)
+    composer('require', "#{source}", "#{source_version}")
     
     # update database
     php("#{doc_root}/#{instance}/maintenance/update.php", '--conf', "#{doc_root}/#{instance}/LocalSettings.php") 
