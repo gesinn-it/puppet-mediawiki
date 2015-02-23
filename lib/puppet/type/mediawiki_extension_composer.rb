e5d8b004550e72b6de8040611415776bcb0f5f03
gesinn.it <github@gesinn.it>
2015-02-23 16:49:55 +0100

    Update and rename lib/puppet/type to lib/puppet/type/mediawiki_extension_composer.rb

---
+++ lib/puppet/type/mediawiki_extension_composer.rb
@@ -0,0 +1,28 @@
+Puppet::Type.newtype(:mediawiki_extension) do
+    @doc = "Manage MediaWiki Extensions via Composer"
+
+    ensurable
+
+    newparam(:name) do
+       desc "The name of the extension to be managed"
+
+       isnamevar
+    end
+
+    newparam(:source) do
+      desc "The location of the Extension to be loaded."
+    end
+    
+    newparam(:source_version) do
+      desc "The version of the Extension to be loaded."
+    end
+ 
+    newparam(:instance) do
+      desc "MediaWiki puppet instance identifier."
+    end
+ 
+    newparam(:doc_root) do
+      desc "MediaWiki base installation path."
+    end
+
+end
