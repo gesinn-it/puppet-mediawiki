561467fe7733d7696dcc3145f4e89040c2f1944f
gesinn.it <github@gesinn.it>
2015-02-24 19:52:27 +0100

    Update mediawiki_extension.rb

--- lib/puppet/provider/mediawiki_extension/mediawiki_extension.rb
+++ lib/puppet/provider/mediawiki_extension/mediawiki_extension.rb
@@ -37,7 +37,7 @@ Puppet::Type.type(:mediawiki_extension).provide(:mediawiki_extension) do
     # Make deploy dir
     File.directory?("#{doc_root}/#{instance}/extensions/#{name}") or Dir.mkdir("#{doc_root}/#{instance}/extensions/#{name}", 0755)
     # Unpack code to Extensions dir
-    tar('-xzf', "/tmp/#{name}.tar.gz", '-C', "#{doc_root}/#{instance}/extensions")
+    tar('-xzf', "/tmp/#{name}.tar.gz", '-C', "#{doc_root}/#{instance}/extensions/#{name}")
     # sync db
     #php("#{doc_root}/#{instance}/maintenance/update.php", '--conf', "#{doc_root}/#{instance}/LocalSettings.php") 
   end
