8eb818c58473f601db8d0a5839f384dd3d4014d4
gesinn.it <github@gesinn.it>
2016-01-07 18:09:20 +0100

    Update mediawiki_extension.rb
    downloading tar.gz from github causes invalid tar.gz sometimes. Let's see if downloading to /tmp will fix this

--- lib/puppet/provider/mediawiki_extension/mediawiki_extension.rb
+++ lib/puppet/provider/mediawiki_extension/mediawiki_extension.rb
@@ -40,13 +40,15 @@ Puppet::Type.type(:mediawiki_extension).provide(:mediawiki_extension) do
     # -L: follow redirects
     # -R: make curl attempt to figure out the timestamp of the remote file, and if that is available make the local file get that same timestamp. 
     # -z: Request a file that has been modified later than the given time and date of an existing file
-    curl('-L', '-R', '-z', "/vagrant_share/tmp/#{name}.tar.gz", '-o', "/vagrant_share/tmp/#{name}.tar.gz", "#{source}")
+    # curl('-L', '-R', '-z', "/vagrant_share/tmp/#{name}.tar.gz", '-o', "/vagrant_share/tmp/#{name}.tar.gz", "#{source}")
+    curl('-L', '-R', '-z', "/tmp/#{name}.tar.gz", '-o', "/tmp/#{name}.tar.gz", "#{source}")
     
     # Make deploy dir
     File.directory?("#{doc_root}/#{instance}/extensions/#{name}") or Dir.mkdir("#{doc_root}/#{instance}/extensions/#{name}", 0755)
     
     # Unpack code to Extensions dir
-    tar('-xzf', "/vagrant_share/tmp/#{name}.tar.gz", "--strip-components=#{strip_components}", '-C', "#{doc_root}/#{instance}/extensions/#{name}")
+    # tar('-xzf', "/vagrant_share/tmp/#{name}.tar.gz", "--strip-components=#{strip_components}", '-C', "#{doc_root}/#{instance}/extensions/#{name}")
+    tar('-xzf', "/tmp/#{name}.tar.gz", "--strip-components=#{strip_components}", '-C', "#{doc_root}/#{instance}/extensions/#{name}")
     
     # sync db
     #php("#{doc_root}/#{instance}/maintenance/update.php", '--conf', "#{doc_root}/#{instance}/LocalSettings.php") 
