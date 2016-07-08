4ff3d76ac2a913001099e20735780739d3f09934
gesinn.it <github@gesinn.it>
2016-07-08 09:57:58 +0200

    test, if composer 1.2.0-RC works

--- manifests/init.pp
+++ manifests/init.pp
@@ -355,7 +355,7 @@ AcOphrnJ
     command_name => 'composer',
     target_dir   => '/usr/local/bin',
     auto_update => false,
-    version => '1.1.3',
+    version => '1.2.0-RC',
     require => Package[$mediawiki::params::packages],
   }
 
